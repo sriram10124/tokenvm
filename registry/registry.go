6191d739e170d738b64765c6056d9fa9ba3940f9
sriram10124 <162477849+sriram10124@users.noreply.github.com>
2024-04-03 21:55:14 +0530

    Update registry.go

--- registry/registry.go
+++ registry/registry.go
@@ -25,6 +25,8 @@ func init() {
 		consts.ActionRegistry.Register(&actions.Transfer{}, actions.UnmarshalTransfer, false),
 
 		// TODO: register action: actions.CreateAsset
+		consts.ActionRegistry.Register(&actions.CreateAssetAsset{}, actions.UnmarshalCreateAsset, false),
+		consts.ActionRegistry.Register(&actions.MintAssetAsset{}, actions.UnmarshalMintAsset, false),
 		// TODO: register action: actions.MintAsset
 		consts.ActionRegistry.Register(&actions.BurnAsset{}, actions.UnmarshalBurnAsset, false),
 		consts.ActionRegistry.Register(&actions.ModifyAsset{}, actions.UnmarshalModifyAsset, false),
@@ -43,3 +45,4 @@ func init() {
 		panic(errs.Err)
 	}
 }
+
